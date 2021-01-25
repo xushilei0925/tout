8c964279981920575dbe0246399eabb75fd89cab
徐世磊 <1964036084@qq.com>
2021-01-25 19:33:39 +0800

    初始化项目结构

--- src/main.js
+++ src/main.js
@@ -2,6 +2,15 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+// 引入样式表文件
+import '@/styles/index.css'
+// 移动端REM
+import 'amfe-flexible'
+
+// 导入vant 库
+import Vant from 'vant'
+import 'vant/lib/index.css'
+Vue.use(Vant)
 
 Vue.config.productionTip = false
 
